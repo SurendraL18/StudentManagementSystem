18c5e6b21705ec6c9f7c93507ae41361d33a9fe3
Surendra Loke <surendra.loke@softlabsgroup.com>
2026-09-02 19:13:38 +0530

    teacherConfiguration

--- StudentManagement.Infrastructure/Persistence/Configurations/StudentConfiguration.cs
+++ StudentManagement.Infrastructure/Persistence/Configurations/StudentConfiguration.cs
@@ -1,6 +1,58 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentManagement.Domain.Entities;
+
 namespace StudentManagement.Infrastructure.Persistence.Configurations
 {
-    public class StudentConfiguration
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
     {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.ToTable("students");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.StudentNumber)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.HasIndex(x => x.StudentNumber)
+                .IsUnique();
+
+            builder.Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(320);
+
+            builder.HasIndex(x => x.Email)
+                .IsUnique();
+
+            builder.Property(x => x.DateOfBirth)
+                .HasColumnType("date")
+                .IsRequired();
+
+            builder.Property(x => x.IsActive)
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAtUtc)
+                .HasColumnName("created_at_utc")
+                .IsRequired();
+
+            builder.Property(x => x.UpdatedAtUtc)
+                .HasColumnName("updated_at_utc")
+                .IsRequired();
+
+        }
+
     }
 }
--- StudentManagement.Infrastructure/Persistence/Configurations/TeacherConfiguration.cs
+++ StudentManagement.Infrastructure/Persistence/Configurations/TeacherConfiguration.cs
@@ -1,7 +1,57 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentManagement.Domain.Entities;
+
 namespace StudentManagement.Infrastructure.Persistence.Configurations
 {
-    public class TeacherConfiguration
+    public class TeacherConfiguration : IEntityTypeConfiguration<Teacher>
     {
+        public void Configure(EntityTypeBuilder<Teacher> builder)
+        {
+            builder.ToTable("teachers");
 
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.EmployeeNumber)
+              .IsRequired()
+              .HasMaxLength(50);
+
+            builder.HasIndex(x => x.EmployeeNumber)
+             .IsUnique();
+
+            builder.Property(x => x.FirstName)
+             .IsRequired()
+             .HasMaxLength(100);
+
+            builder.Property(x => x.LastName)
+             .IsRequired()
+             .HasMaxLength(100);
+
+            builder.Property(x => x.Email)
+             .IsRequired()
+             .HasMaxLength(320);
+
+            builder.HasIndex(x => x.Email)
+              .IsUnique();
+
+            builder.Property(x => x.Department)
+             .IsRequired()
+             .HasMaxLength(150);
+
+            builder.Property(x => x.IsActive)
+              .IsRequired();
+
+            builder.Property(x => x.CreatedAtUtc)
+              .HasColumnName("created_at_utc")
+              .IsRequired();
+
+            builder.Property(x => x.UpdatedAtUtc)
+               .HasColumnName("updated_at_utc")
+               .IsRequired();
+
+        }
     }
 }
